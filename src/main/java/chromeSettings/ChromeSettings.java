b86c1a1bad3f921e27f83aa6358a1f6e488c545b
Nazar <hmyzn07@gmail.com>
2018-02-04 15:13:06 +0200

    ChromeSettings added

--- src/main/java/chromeSettings/ChromeSettings.java
+++ src/main/java/chromeSettings/ChromeSettings.java
@@ -1,4 +1,25 @@
 package chromeSettings;
 
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class ChromeSettings {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp () {
+        System.setProperty("webdriver.chrome.driver","C:\\chromeDriver\\chromedriver.exe");
+        driver.get("http://ek.ua/");
+        System.out.println("Test Start");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    };
+    @After
+    public void tearDown () {
+        driver.close();
+        System.out.println("Test Complete");
+    }
 }
